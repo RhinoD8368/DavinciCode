5fcb879768464e29cc0d8eb00cc3fa871b295b14
rhinod <rhinod@naver.com>
2026-03-16 22:46:33 +0900

    test

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import AuthLayout from './components/layouts/authLayout/AuthLayout'; 
 import Login from './pages/loginPage/Login';
+import Join from './pages/joinPage/Join';
+
 function App() {
 
   return (
     <Routes> 
       <Route path='/' element={<AuthLayout />} >
         <Route index element={ <Login /> } />
+        <Route path="join" element={ <Join /> } />
       </Route>
     </Routes>
   )
---
+++ FE/src/pages/joinPage/Join.jsx
@@ -0,0 +1,14 @@
+import styles from './Join.module.css';
+import { VStack, HStack } from '../../components/layouts/stackLayout/Stack';
+
+const Join = () => {
+    return (
+        <div>
+            <VStack>
+                Input
+            </VStack>
+        </div>
+    );
+}
+
+export default Join;
