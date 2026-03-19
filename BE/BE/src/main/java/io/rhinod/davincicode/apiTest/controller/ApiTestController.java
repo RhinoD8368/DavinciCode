c18bb1ea86d5d5374fbde092e3a9285bad719629
rhinod <rhinod@naver.com>
2026-03-19 23:37:21 +0900

    test

--- BE/BE/src/main/java/io/rhinod/davincicode/apiTest/controller/ApiTestController.java
+++ BE/BE/src/main/java/io/rhinod/davincicode/apiTest/controller/ApiTestController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.rhinod.davincicode.apiTest.service.ApiTestService;
